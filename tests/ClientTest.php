8292fa85c1af13dff075597fbb576267b3ca1446
Selim Saygi <selim.saygi@trustedshops.de>
2024-07-17 11:35:54 +0200

    test: Add some tests for base client functionality

---
+++ tests/ClientTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+use TRSTD\COT\Client;
+use TRSTD\COT\Exception\RequiredParameterMissingException;
+use TRSTD\COT\AuthStorageInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Cache\CacheItemPoolInterface;
+
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
+use TRSTD\COT\Token;
+
+final class ClientTest extends TestCase
+{
+    private const TEST_ID_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4iLCJpYXQiOjE1MTYyMzkwMjIsImN0Y19pZCI6IjEyMzQ1Njc4OTAifQ.vCzpQFib8wXKVzHBSuBkaqskUc3Q7RyaLvJ5HkmI4zA";
+
+    /**
+     * @covers \TRSTD\COT\Client::__construct
+     */
+    public function testConstructorThrowsRequiredParameterMissingExceptionForMissingParameters()
+    {
+        $mockAuthStorage = $this->createMock(AuthStorageInterface::class);
+        $this->expectException(RequiredParameterMissingException::class);
+        new Client('', '', '', $mockAuthStorage);
+    }
+
+    /**
+     * @covers \TRSTD\COT\Client::__construct
+     */
+    public function testConstructorThrowsTypeErrorForNullAuthStorage()
+    {
+        $this->expectException(TypeError::class);
+        new Client('ABC', 'ABC', 'ABC', null);
+    }
+
+    /**
+     * @covers \TRSTD\COT\Client::handleCallback
+     */
+    public function testHandleCallbackWithCode()
+    {
+        $_GET['code'] = 'testCode';
+        $_SERVER['HTTP_HOST'] = 'testHost';
+        $_SERVER["REQUEST_URI"] = 'testUri';
+        $_COOKIE["TRSTD_CV"] = null;
+        $_COOKIE["TRSTD_CC"] = null;
+
+        $expectedRequests = [
+            function ($method, $url, $options): MockResponse {
+                $this->assertSame('POST', $method);
+                $this->assertSame('https://auth-qa.trustedshops.com/auth/realms/myTS-QA/protocol/openid-connect/token', $url);
+                $this->assertArrayHasKey('body', $options);
+                $this->assertArrayHasKey('headers', $options);
+
+                return new MockResponse('{"id_token":"' . self::TEST_ID_TOKEN . '", "refresh_token":"testRefreshToken", "access_token":"testAccessToken"}');
+            },
+        ];
+
+        $mockedHttpClient = new MockHttpClient($expectedRequests);
+        $mockedLogger = $this->createMock(LoggerInterface::class);
+        $mockedCacheItemPool = $this->createMock(CacheItemPoolInterface::class);
+        $mockedAuthStorage = $this->createMock(AuthStorageInterface::class);
+        $mockedAuthStorage->expects($this->once())->method('set')->with("1234567890", new Token(self::TEST_ID_TOKEN, 'testRefreshToken', 'testAccessToken'));
+
+        $client = new Client('TSID', 'CLIENT_ID', 'CLIENT_SECRET', $mockedAuthStorage);
+        $client->setLogger($mockedLogger);
+        $client->setHttpClient($mockedHttpClient);
+        $client->setCacheItemPool($mockedCacheItemPool);
+
+        $client->handleCallback();
+    }
+}
